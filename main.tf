ab03225c4db70d689ba7667b114089769d182771
sankusinha <47401612+sankusinha@users.noreply.github.com>
2020-08-28 02:18:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 provider "aws" {
   region = "ap-southeast-1"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
 }
+
+variable "AWS_ACCESS_KEY" {}
+variable AWS_SECRET_KEY {}
 
 resource "aws_instance" "tf-cloud-ec2" {
   ami = "ami-0cd31be676780afa7"
